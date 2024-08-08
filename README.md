84f1fa25b76df416711068f50ee80927230d4864
manishTerminal <iammanish61@gmail.com>
2024-08-08 19:22:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Making a Netflix clone with GPT intigration
+
+work in progress...
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1>Netflix</h1>
+    </>
+  )
+}
+
+export default App
